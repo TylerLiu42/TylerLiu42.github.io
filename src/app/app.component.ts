2e410e62df2754c44a2a3040f86ccc42181e9b73
Tyler Liu <tgqliu@uwaterloo.ca>
2020-05-18 15:48:23 -0400

    Use Angular CLI, finish intro

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IntroComponent } from './intro/intro.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tyler-website';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    HeaderComponent,
+    ExperiencesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
